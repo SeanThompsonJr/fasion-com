e261cccf92dd25f85f758fc80e24708e54395bb6
Sean Thompson JR <65671096+SeanThompsonJr@users.noreply.github.com>
2024-11-19 17:50:12 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// site im making - https://www.figma.com/design/4h1kiPjpIcNoNgEws6kYWi/E-commerce-Website-Template-(Freebie)-(Community)?node-id=0-1&node-type=canvas&t=ckieJJSte9faC9fj-0
+//! npm run dev
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black">test</div>
+    </>
+  );
+}
+
+export default App;
